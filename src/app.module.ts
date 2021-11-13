85fa32a341a073c85b2674cf38052f09580ae537
carmona44 <dcacarmona44@gmail.com>
2021-11-13 18:43:26 +0100

    feat: init mongo mikroOrm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      entities: ['./dist/entities'],
+      entitiesTs: ['./src/entities'],
+      dbName: 'padiary',
+      type: 'mongo',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
