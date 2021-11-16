cd36006e9dd31a2c32d3a0b93d58d30c909beefe
carmona44 <dcacarmona44@gmail.com>
2021-11-16 20:45:20 +0100

    feat: remove REST user endpoints

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,11 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { User } from './users.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [MikroOrmModule.forFeature({ entities: [User] })],
-  controllers: [UsersController],
   providers: [UsersService, UsersResolver]
 })
 export class UsersModule {}
